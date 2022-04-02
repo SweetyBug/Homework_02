0b069075080c0feca9a1757714e8de29df52abbc
Maxxx <maxim_terekhin@bk.ru>
2022-04-02 10:39:33 +0300

    Add folder for task 52

---
+++ Task_47/Program.cs
@@ -0,0 +1,32 @@
+﻿void CreatArray(double[,] mas)
+{
+    for(int i = 0; i < mas.GetLength(0); i++)
+    {
+        for(int j = 0; j < mas.GetLength(1); j++)
+        {
+            mas[i, j] = new Random().NextDouble() * 20;
+        }
+    }
+}
+
+void PrintArray(double[,] mas)
+{
+    for(int k = 0; k < mas.GetLength(0); k++)
+    {
+        for(int m = 0; m < mas.GetLength(1); m++)
+        {
+            Console.Write("{0:0.00}", mas[k, m]);
+            Console.Write("; ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк в массиве: ");
+int str = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[str, column];
+CreatArray(array);
+PrintArray(array);
---
+++ Task_49/Program.cs
@@ -0,0 +1,48 @@
+﻿void CreatArray(int[,] mas)
+{
+    for(int i = 0; i < mas.GetLength(0); i++)
+    {
+        for(int j = 0; j < mas.GetLength(1); j++)
+        {
+            mas[i, j] = new Random().Next(10, 100);
+        }
+    }
+}
+
+void PrintArray(int[,] mas)
+{
+    for(int k = 0; k < mas.GetLength(0); k++)
+    {
+        for(int m = 0; m < mas.GetLength(1); m++)
+        {
+            Console.Write(mas[k, m] + "; ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReplacementArray(int[,] mas)
+{
+    for(int i = 0; i < mas.GetLength(0); i++)
+    {
+        for(int j = 0; j < mas.GetLength(1); j++)
+        {
+            if (i % 2 == 1 && j % 2 == 1)
+            {
+                mas[i, j] *= mas[i, j];
+            }
+        }
+    }
+}
+
+Console.Write("Введите количество строк в массиве: ");
+int str = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[str, column];
+CreatArray(array);
+PrintArray(array);
+Console.WriteLine();
+ReplacementArray(array);
+PrintArray(array);
